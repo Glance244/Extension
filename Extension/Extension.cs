42c5d367a949208f50e562c3d7a5019550bddb7b
User <User@DESKTOP-SV3KE10>
2023-08-24 09:39:19 +0200

    Add project files.

---
+++ Extension/Extension.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace Extension
+{
+    public static class Extension
+    {
+        public static int WordCount(this string s)
+        {
+            string[] words = s.Split(new char[] {' ','.','?','!'}, StringSplitOptions.RemoveEmptyEntries);
+            return words.Length;
+        }
+        public static int Parse(this string s)
+        {
+            return int.TryParse(s, out int i)? i:0;
+        }
+    }
+}
---
+++ Extension/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extension
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "John is afraid of dogs,  He should run now!!";
+            Console.WriteLine(s);
+            Console.WriteLine("Words: "+s.WordCount());
+
+
+            Console.Write("Enter John's age: ");
+            int iAge = Console.ReadLine().Parse();
+            Console.WriteLine("Age: "+iAge);
+            Console.Write("\nPress any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
